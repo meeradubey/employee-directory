601ebe8b30be1deb1ce588bbfb4c9eb68aff8f2b
meera <meeradubey97>
2019-12-02 10:32:05 -0800

    for heroku deployment

--- file/src/App.js
+++ file/src/App.js
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import EmployeeCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import Employee from "./friends.json";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    Employee
   };
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    const Employee = this.state.Employee.filter(friend => Employee.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    this.setState({ Employee });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+        <Title>Employee List</Title>
+        {this.state.Employee.map(Employee => (
+          <EmployeeCard
+            removeEmployee={this.EmployeeFriend}
+            id={Employee.id}
+            key={Employee.id}
+            name={Employee.name}
+            image={Employee.image}
+            occupation={Employee.occupation}
+            location={Employee.location}
           />
         ))}
       </Wrapper>
--- file/src/components/EmployeeCard/index.js
+++ file/src/components/EmployeeCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+function EmployeeCard(props) {
   return (
     <div className="card">
       <div className="img-container">
@@ -20,11 +20,11 @@ function FriendCard(props) {
           </li>
         </ul>
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
+      <span onClick={() => props.removeEmployee(props.id)} className="remove">
         𝘅
       </span>
     </div>
   );
 }
 
-export default FriendCard;
+export default EmployeeCard;
